6f1269fb0fa32c32c9fbe4a3f9a7a7842a8a24b5
teck157713 <teck.157713@hotmail.com>
2019-04-02 14:15:17 +0800

    Add files via upload

--- postgreStructure/app.py
+++ postgreStructure/app.py
@@ -7,17 +7,62 @@ import base64
 import requests
 import json
 import datetime
+import argparse
+import logging
 from PIL import Image
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+logger = logging.getLogger(__name__)
 
-
+repo_path = os.path.curdir
 app = Flask(__name__)
 app.debug = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://teck:password@127.0.0.1:5000/smuqueue'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://teck:password@/smuqueue?host=/cloudsql/smuqueue:asia-southeast1:smuqueue'
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://teck:password@127.0.0.1:5432/smuqueue'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import INFO, TIME_LOG
+from localize_image import YOLODetector
+
+def microsoftapi(img):
+	#Microsoft Face API
+	apikey = "e7290e03190c41148ec555889daf2f64"
+	assert apikey
+	api_url = "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/"
+	analyse_api = api_url + "analyze"
+	image_data = img
+	headers = {"Ocp-Apim-Subscription-Key": apikey,
+	'Content-Type': 'application/octet-stream'}
+	params = {'visualFeatures':'Categories,Description,Color,Objects,Faces'}
+	response = requests.post(
+		analyse_api, headers=headers, params=params, data=image_data)
+	response.raise_for_status()
+	analysis = response.json()
+	#image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+	people = 0
+	for i in analysis['objects']:
+		if i['object'] == 'person':
+			people += 1
+	describepeople = []
+	for i in analysis['faces']:
+		describepeople.append(i['gender'] + ' ' + str(i['age']))
+	tags = analysis['description']['tags']
+	return[people, describepeople, tags]
+
+def neuralnetwork(imgresult):
+	image = Image.open(imgresult)
+	yolo_model = YOLODetector(score_threshold=float(0.3),
+                              iou_threshold=float(0.45))
+	detected_image = yolo_model.detect_image(image)
+	return detected_image[1]
+
+@app.route('/main/', methods=['GET'])
+def givedefault():
+	return jsonify('Welcome to this retarded page.')
 
 @app.route('/newvendor/', methods=['POST'])
 def add_vendor():
@@ -27,72 +72,35 @@ def add_vendor():
 	new_vendor = INFO(Vendor=Vendor, Menu=base64.b64encode(Menu), Queue_Image=base64.b64encode(Queue_Image))
 	db.session.add(new_vendor)
 	db.session.commit()
-	#Microsoft Face API
-	apikey = "e7290e03190c41148ec555889daf2f64"
-	assert apikey
-
-	api_url = "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/"
-
-	analyse_api = api_url + "analyze"
-
-	image_data = Queue_Image
-	headers = {"Ocp-Apim-Subscription-Key": apikey,
-	'Content-Type': 'application/octet-stream'}
-	params = {'visualFeatures':'Categories,Description,Color,Objects,Faces'}
-
-	response = requests.post(
-		analyse_api, headers=headers, params=params, data=image_data)
-	response.raise_for_status()
-
-	analysis = response.json()
-	print(analysis)
-	image_caption = analysis["description"]["captions"][0]["text"].capitalize()
-
-	people = 0
-	for i in analysis['objects']:
-		if i['object'] == 'person':
-			people += 1
-	describepeople = ""
-	for i in analysis['faces']:
-		describepeople += i['gender'] + " " + str(i['age']) + "\n"
-
-	print("Number of people detected: " + str(people))
-	print(describepeople)
-	tags = analysis['description']['tags']
-	#Microsoft Face API Ends
-	new_log = TIME_LOG(Vendor, datetime.datetime.now(), people, describepeople, " ".join(tags))
+	res = microsoftapi(Queue_Image)
+	res2 = neuralnetwork(request.files.get('Queue_Image'))
+	new_log = TIME_LOG(info_id=new_vendor.id, Time=datetime.datetime.now(), Queue_Length=res2, Audience=res[1], Activities_Of_Interest=res[2])
 	db.session.add(new_log)
 	db.session.commit()
-	
-	return jsonify(new_log)
+	return jsonify(new_vendor.serialize())
 
 @app.route('/getvendors/', methods=['GET'])
 def get_vendors():
 	all_vendors = INFO.query.all()
-	return jsonify(all_vendors.data)
+	return jsonify([a.serialize() for a in all_vendors])
 
 @app.route('/getlogs/', methods=['GET'])
 def get_logs():
 	all_logs = TIME_LOG.query.all()
-	return jsonify(all_logs.data)
+	return jsonify([l.serialize() for l in all_logs])
 
 @app.route('/getvendor/', methods=['POST'])
 def get_vendor():
 	Vendor = request.form['Vendor']
-	all_vendors = INFO.query.filter(INFO.Vendor==Vendor).with_entities(INFO.Vendor,INFO.Menu).all()
-
-	#return infos_schema.jsonify(all_vendors)
-	return jsonify(all_vendors.data)
+	all_vendors = INFO.query.filter(INFO.Vendor==Vendor).all()
+	return jsonify([a.serialize() for a in all_vendors])
 
 @app.route('/SMUqueue/<vendor_name>', methods=['DELETE'])
 def deleteVendor(vendor_name):
 	delvendor = INFO.query.filter(INFO.Vendor==vendor_name).one()
 	db.session.delete(delvendor)
 	db.session.commit()
-	deltime = TIME_LOG.query.filter(TIME_LOG.Vendor==vendor_name).all()
-	db.session.delete(deltime)
-	db.session.commit()
-	return jsonify(delvendor)
+	return jsonify(delvendor.serialize())
 
 @app.route('/SMUqueue/<vendor_name>', methods=['PUT'])
 def update_name(vendor_name):
@@ -100,10 +108,7 @@ def update_name(vendor_name):
 	updatevendor = INFO.query.filter(INFO.Vendor==vendor_name).first()
 	updatevendor.Vendor = update_vendor_name
 	db.session.commit()
-	updatetime = TIME_LOG.query.filter(TIME_LOG.Vendor==vendor_name).all()
-	updatetime.Vendor = update_vendor_name
-	db.session.commit()
-	return jsonify(updatevendor)
+	return jsonify(updatevendor.serialize())
 
 @app.route('/SMUqueue/<vendor_name>', methods=['PUT'])
 def update_menu(vendor_name):
@@ -111,7 +116,7 @@ def update_menu(vendor_name):
 	updatemenu = INFO.query.filter(INFO.Vendor==vendor_name).first()
 	updatemenu.Menu = updated_menu
 	db.session.commit()
-	return jsonify(updatemenu)
+	return jsonify(updatemenu.serialize())
 
 @app.route('/updatequeue/<vendor_name>', methods=['PUT'])
 def update_queue(vendor_name):
@@ -120,45 +125,16 @@ def update_queue(vendor_name):
 	updatequeue = INFO.query.filter(INFO.Vendor==vendor_name).first()
 	updatequeue.Queue_Image = updated_queue
 	db.session.commit()
-
-	apikey = "e7290e03190c41148ec555889daf2f64"
-	assert apikey
-
-	api_url = "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/"
-
-	analyse_api = api_url + "analyze"
-
-	image_data = newimage
-	headers = {"Ocp-Apim-Subscription-Key": apikey,
-	'Content-Type': 'application/octet-stream'}
-	params = {'visualFeatures':'Categories,Description,Color,Objects,Faces'}
-
-	response = requests.post(
-		analyse_api, headers=headers, params=params, data=image_data)
-	response.raise_for_status()
-
-	analysis = response.json()
-	print(analysis)
-	image_caption = analysis["description"]["captions"][0]["text"].capitalize()
-
-	people = 0
-	for i in analysis['objects']:
-		if i['object'] == 'person':
-			people += 1
-	describepeople = ""
-	for i in analysis['faces']:
-		describepeople += i['gender'] + " " + str(i['age']) + "\n"
-
-	print("Number of people detected: " + str(people))
-	print(describepeople)
-	tags = analysis['description']['tags']
-	#Microsoft Face API Ends
-	new_log = TIME_LOG(vendor_name, datetime.datetime.now(), people, describepeople, " ".join(tags))
+	res = microsoftapi(newimage)
+	res2 = neuralnetwork(request.files.get('updated_queue'))
+	new_log = TIME_LOG(info_id=updatequeue.id, Time=datetime.datetime.now(), Queue_Length=res2, Audience=res[1], Activities_Of_Interest=res[2])
 	db.session.add(new_log)
 	db.session.commit()
-	return jsonify(new_log)
-
-
+	return jsonify(new_log.serialize())
 
 if __name__ == '__main__':
+	#app.run(host='127.0.0.1', port=8080, debug=True)
 	app.run(debug=True)
+
+
+
--- postgreStructure/models.py
+++ postgreStructure/models.py
@@ -1,4 +1,5 @@
 from app import db
+import datetime
 
 class INFO(db.Model):
 	__tablename__='info'
@@ -6,42 +7,55 @@ class INFO(db.Model):
 	Vendor = db.Column(db.String(80), unique=True, nullable=False)
 	Menu = db.Column(db.LargeBinary, nullable=True)
 	Queue_Image = db.Column(db.LargeBinary, nullable=True)
+	timelogs = db.relationship('TIME_LOG', back_populates='info', cascade='all', lazy=True, uselist=True)
 
-	def __init__(self, Vendor, Menu, Queue_Image):
+	def __init__(self, Vendor, Menu=None, Queue_Image=None, timelogs=None):
 		self.Vendor = Vendor
+		Menu = [] if Menu is None else Menu
 		self.Menu = Menu
+		Queue_Image = [] if Queue_Image is None else Queue_Image
 		self.Queue_Image = Queue_Image
+		timelogs = [] if timelogs is None else timelogs
+		self.timelogs = timelogs
 
 	def __repr__(self):
 		return '<id {}>'.format(self.id)
     
-	def serializeinfo(self):
+	def serialize(self):
 		return {
+			'id' : self.id,
             'Vendor': self.Vendor,
-			'Menu': self.Menu,
-            'Queue_Image': self.Queue_Image
+			'Menu': str(self.Menu),
+            'Queue_Image': str(self.Queue_Image),
+			'timelogs' : [t.serialize() for t in self.timelogs]
         }
 
 class TIME_LOG(db.Model):
+	__tablename__= 'timelog'
+	__table_args__ = (
+		db.UniqueConstraint('info_id', 'Time', name='composite_unique'),
+	)
 	id = db.Column(db.Integer, primary_key=True)
-	Vendor = db.Column(db.String(80), nullable=False)
-	Time = db.Column(db.DateTime, nullable=False)
-	Queue_Length = db.Column(db.Integer, nullable=True)
-	Audience = db.Column(db.String(200), nullable=True)
-	Activities_Of_Interest = db.Column(db.String(500), nullable=True)
+	Time = db.Column(db.DateTime, default=datetime.datetime.now().date(), onupdate=datetime.datetime.now().date())
+	Queue_Length = db.Column(db.Integer, nullable=False)
+	Audience = db.Column(db.String(200), nullable=False)
+	Activities_Of_Interest = db.Column(db.String(500), nullable=False)
+	info_id = db.Column(db.Integer, db.ForeignKey('info.id'), nullable=False)
+	info = db.relationship('INFO', back_populates='timelogs')
 
-	def __init__(self, Vendor, Time, Queue_Length, Audience, Activities_Of_Interest):
-		self.Vendor = Vendor
+	def __init__(self, Time, Queue_Length, Audience, Activities_Of_Interest, info_id):
 		self.Time = Time
 		self.Queue_Length = Queue_Length
 		self.Audience = Audience
 		self.Activities_Of_Interest = Activities_Of_Interest
+		self.info_id = info_id
     
-	def serializetimelog(self):
+	def serialize(self):
 		return {
-            'Vendor': self.Vendor,
+			'id' : self.id,
             'Time': self.Time,
             'Queue_Length': self.Queue_Length,
             'Audience': self.Audience,
-            'Activities_Of_Interest': self.Activities_Of_Interest
+            'Activities_Of_Interest': self.Activities_Of_Interest,
+			'info_id' : self.info_id
         }
